8e69e3ef9d5cfce90985a2af713397cec1ee2404
Arun Kumar <frostarun@gmail.com>
2020-12-05 16:19:38 +0530

    [HM-2] Added logger and database preload scripts

--- src/main/java/com/froststudio3e/housemgmt/HousemgmtApplication.java
+++ src/main/java/com/froststudio3e/housemgmt/HousemgmtApplication.java
@@ -1,13 +1,52 @@
 package com.froststudio3e.housemgmt;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.dao.DuplicateKeyException;
+
+import com.froststudio3e.housemgmt.models.ERole;
+import com.froststudio3e.housemgmt.models.House;
+import com.froststudio3e.housemgmt.models.Role;
+import com.froststudio3e.housemgmt.repository.HouseRepository;
+import com.froststudio3e.housemgmt.repository.RoleRepository;
 
 @SpringBootApplication
 public class HousemgmtApplication {
+	
+	 private final Logger LOGGER = LoggerFactory.getLogger(this.getClass());
+
+	@Autowired
+	private RoleRepository roleRepository;
+
+	@Autowired
+	private HouseRepository houseRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(HousemgmtApplication.class, args);
+	}
+
+	@Bean
+	InitializingBean sendDatabase() {
+		return () -> {
+			try {
+				roleRepository.save(new Role(ERole.ROLE_ADMIN));
+				roleRepository.save(new Role(ERole.ROLE_MODERATOR));
+				roleRepository.save(new Role(ERole.ROLE_USER));
+				houseRepository.save(House.builder().name("mercury").build());
+				houseRepository.save(House.builder().name("venus").build());
+				houseRepository.save(House.builder().name("earth").build());
+				houseRepository.save(House.builder().name("mars").build());
+			} catch (DuplicateKeyException e) {
+				LOGGER.info("Preload INFO : Already preloaded data available");
+			} catch (Exception e) {
+				LOGGER.error("Error in inserting Preload data");
+			}
+		};
 	}
 
 }
--- src/main/java/com/froststudio3e/housemgmt/models/House.java
+++ src/main/java/com/froststudio3e/housemgmt/models/House.java
@@ -1,6 +1,7 @@
 package com.froststudio3e.housemgmt.models;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import lombok.Builder;
@@ -13,5 +14,7 @@ public class House {
 
 	@Id
 	private String id;
+	
+	@Indexed(unique = true)
 	private String name;
 }
